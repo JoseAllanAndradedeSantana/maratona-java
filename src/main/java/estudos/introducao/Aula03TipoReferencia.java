85e80a19435e059c0839ae46697667bcaeb4b95d
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-22 18:28:08 -0300

    reference type

---
+++ src/main/java/estudos/introducao/Aula03TipoReferencia.java
@@ -0,0 +1,12 @@
+package estudos.introducao;
+
+public class Aula03TipoReferencia {
+    public static void main(String[] args) {
+        String firstName = "Jose Allan";
+        String lastName = "Andrade de Santana";
+        String address = "Rua Americo Vespucio";
+        Double salario = 2800.00;
+        String role = "Desenvolvedor Pleno Java";
+        System.out.println("Nome: " + firstName+" " + lastName + "\n" + address + "\n" + "R$"+salario + " Cargo: " +role);
+    }
+}
