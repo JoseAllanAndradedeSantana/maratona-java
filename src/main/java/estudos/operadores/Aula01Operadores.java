e61fa005b3d17d11200f86aa053bbade44094202
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-23 07:42:30 -0300

    Operadores Aritimeticos.

---
+++ src/main/java/estudos/operadores/Aula01Operadores.java
@@ -0,0 +1,12 @@
+package estudos.operadores;
+
+public class Aula01Operadores {
+    //+ - * / %
+    public static void main(String[] args) {
+        int number1 = 28;
+        int number2 = 12;
+        int result = number1 + number2;
+        System.out.println("Valor " + result);
+
+    }
+}
