5aa9bfbf518eb0c1f24d0fc30831ae39c8b90f97
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-26 22:34:17 -0300

    Estrutura condicionais else.

---
+++ src/main/java/estudos/operadores/Aula05EstruturaCondicionais02.java
@@ -0,0 +1,14 @@
+package estudos.operadores;
+
+public class Aula05EstruturaCondicionais02 {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age < 15){
+            System.out.println("Infantil");
+        } else if(age >= 15 && age < 18){
+            System.out.println("Juvenil");
+        }else {
+            System.out.println("Adulto");
+        }
+    }
+}
