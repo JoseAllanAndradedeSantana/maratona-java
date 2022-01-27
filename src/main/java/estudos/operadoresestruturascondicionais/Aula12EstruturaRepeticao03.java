ade8111663f239dda310e8d23ebf3c4fad351397
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-27 09:22:16 -0300

    Estrutura de Repeticao numeros de parcelas.

---
+++ src/main/java/estudos/operadoresestruturascondicionais/Aula12EstruturaRepeticao03.java
@@ -0,0 +1,15 @@
+package estudos.operadoresestruturascondicionais;
+
+public class Aula12EstruturaRepeticao03 {
+    public static void main(String[] args) {
+        double valorCarro = 40000;
+        for (int parcela = 1; parcela < valorCarro ; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if(valorParcela < 1000){
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+
+        }
+    }
+}
