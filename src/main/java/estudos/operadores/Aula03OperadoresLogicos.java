016c7d0e97da2a117847d273bdd2ee7bea736089
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-26 22:33:24 -0300

    Operadores Logicos.

---
+++ src/main/java/estudos/operadores/Aula03OperadoresLogicos.java
@@ -0,0 +1,21 @@
+package estudos.operadores;
+
+public class Aula03OperadoresLogicos {
+    // AND && OR || NOT !
+    public static void main(String[] args) {
+        int age = 32;
+        double salario = 3000;
+        boolean isDentroLei = age > 35 && salario > 4612;
+        System.out.println(isDentroLei);
+
+        double valorContaCorrente = 5000;
+        double valorContaPoupanca = 15000;
+        double valorPlaystation = 5000;
+        boolean isPlaystationCompravel = valorContaCorrente > valorPlaystation || valorContaPoupanca > valorPlaystation;
+        System.out.println(isPlaystationCompravel);
+
+        double bonus = 1000;
+        bonus += 1;
+        System.out.println(bonus);
+    }
+}
