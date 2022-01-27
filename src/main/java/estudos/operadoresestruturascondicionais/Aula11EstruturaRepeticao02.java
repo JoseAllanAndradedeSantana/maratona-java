455d03c2bb3c1ee9fab422908adfefa61e2b98fa
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-27 09:00:20 -0300

    Estrutura de Repeticao numeros pares.

---
+++ src/main/java/estudos/operadoresestruturascondicionais/Aula11EstruturaRepeticao02.java
@@ -0,0 +1,12 @@
+package estudos.operadoresestruturascondicionais;
+
+public class Aula11EstruturaRepeticao02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10000 ; i++) {
+            if( i % 2 == 0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
