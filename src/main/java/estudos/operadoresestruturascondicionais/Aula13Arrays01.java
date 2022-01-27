1cd1742939fa69d3910a3b62096e460c3182e519
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-27 09:22:36 -0300

    Arrays01.

---
+++ src/main/java/estudos/operadoresestruturascondicionais/Aula13Arrays01.java
@@ -0,0 +1,15 @@
+package estudos.operadoresestruturascondicionais;
+
+public class Aula13Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 32;
+        idades[1] = 28;
+        idades[2] = 13;
+
+        for (int i = 0; i <3 ; i++) {
+            System.out.println(idades[i]);
+
+        }
+    }
+}
