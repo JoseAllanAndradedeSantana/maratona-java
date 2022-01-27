e3a86fe9bfafa25617eb8049ea6a2607c1a1663e
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-26 22:33:55 -0300

    Estrutura condicionais.

---
+++ src/main/java/estudos/operadores/Aula04EstruturaCondicionais.java
@@ -0,0 +1,15 @@
+package estudos.operadores;
+
+public class Aula04EstruturaCondicionais {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age >= 18){
+            System.out.println("Liberado para compra");
+        }
+
+        boolean isAutorizado = age < 17;
+        if(!isAutorizado){
+            System.out.println("If negacao !!!");
+        }
+    }
+}
