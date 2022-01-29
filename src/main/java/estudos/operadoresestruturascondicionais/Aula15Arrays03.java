53dfd9f5492edaca9c40f1abe906ab4dba4c948d
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-29 11:46:01 -0300

    Arrays03.

---
+++ src/main/java/estudos/operadoresestruturascondicionais/Aula15Arrays03.java
@@ -0,0 +1,10 @@
+package estudos.operadoresestruturascondicionais;
+
+public class Aula15Arrays03 {
+    public static void main(String[] args) {
+        int[] numeros = new int[]{1,2,3,4,5};
+        for (int num:numeros) {
+            System.out.println(num);
+        }
+    }
+}
