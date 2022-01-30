5e6eac6423303b2979370c30331c46c2285673bd
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-30 11:25:23 -0300

    OO Metodos.

---
+++ src/main/java/estudos/metodos/domain/Calculadora.java
@@ -0,0 +1,10 @@
+package estudos.metodos.domain;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        int result = 10+10;
+        System.out.println(result);
+        System.out.println("Finalizando metodos de somar");
+    }
+}
---
+++ src/main/java/estudos/metodos/test/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package estudos.metodos.test;
+
+import estudos.metodos.domain.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.somaDoisNumeros();
+        System.out.println("Finalizando Calculadora Test");
+    }
+}
