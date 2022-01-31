d03fca12e1011ca49a3fd7b4c96b1dc9e26747a8
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-31 14:56:21 -0300

    OO Associacao.

---
+++ src/main/java/estudos/associacao/domain/Escola.java
@@ -0,0 +1,34 @@
+package estudos.associacao.domain;
+
+public class Escola {
+    private String nome;
+    private Professor[] professores;
+
+    public Escola(String nome) {
+        this.nome = nome;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        if(professores == null) return;
+        for (Professor prof:professores) {
+            System.out.println(prof.getNome());
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Professor[] getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(Professor[] professores) {
+        this.professores = professores;
+    }
+}
