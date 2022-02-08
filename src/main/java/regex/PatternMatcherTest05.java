b701b34cc9899ba74e91938ad463beac92640677
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-02-08 10:38:41 -0300

    Regex.

---
+++ src/main/java/regex/PatternMatcherTest05.java
@@ -0,0 +1,30 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest05 {
+    public static void main(String[] args) {
+        // \d = return all digits
+        // \D = return not all digits
+        // \s = return all space in white
+        // \S = not return space in white
+        // \w = a-zA-Z, digitis _
+        // \W = inverse \w
+        //[] [a-zA-Z]
+        // ? zero ou uma
+        // * zero ou mais
+        // + uma ou mais
+        //{nm} n p m
+        //$ fim da linha
+        String regex = "([a-zA-Z0-9\\._-])+@([a-z])+(\\.([a-z])+)+";
+        String texto = "allan_santana2810@hotmail.com, allan_santana2810@icloud.com.br, teste";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+
+        while(matcher.find()){
+            System.out.print(matcher.start()+" "+  matcher.group()+ "\n");
+        }
+    }
+}
