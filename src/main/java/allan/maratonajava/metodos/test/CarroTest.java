255b27203cc491aab23192b3954f0c2f748a17df
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-02-15 16:07:50 -0300

    stream.

---
+++ src/main/java/allan/maratonajava/metodos/test/CarroTest.java
@@ -0,0 +1,10 @@
+package allan.maratonajava.metodos.test;
+
+import allan.maratonajava.metodos.domain.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("Fox",220);
+        c1.imprime();
+    }
+}
