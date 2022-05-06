38d2c6c58c0a322ef0f98d7c843f72f4b2560e0d
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-05-06 14:36:44 -0300

    metodos

---
+++ src/main/java/com/jaas/metodos/test/MensagemTest.java
@@ -0,0 +1,12 @@
+package com.jaas.metodos.test;
+
+import com.jaas.metodos.domain.Mensagem;
+
+public class MensagemTest {
+    public static void main(String[] args){
+        Mensagem.obterMensagem(5);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(22);
+        Mensagem.obterMensagem(25);
+    }
+}
