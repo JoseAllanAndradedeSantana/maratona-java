6f7dfc0d650ac4244f6bb0bf701f1dbee9e35b20
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-03-04 08:40:33 -0300

    Aula maratonajava

---
+++ src/main/java/allan/maratonajava/introducao/Comentarios.java
@@ -0,0 +1,16 @@
+package allan.maratonajava.introducao;
+
+/**
+ * comentario javaDoc
+ */
+public class Comentarios {
+    // comentario de uma linha
+    /*
+        comentario
+        de multiplas
+        linhas
+     */
+    public static void main(String[] args){
+
+    }
+}
